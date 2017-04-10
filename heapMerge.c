69ae9d5dd1acb9302496c0a62e3a85207a916340
Henrique de Almeida <henriquealmeida1492@gmail.com>
2017-04-10 10:38:49 -0300

    Implement the input mechanism for heapMerge

--- heapMerge.c
+++ heapMerge.c
@@ -1,3 +1,44 @@
-int heapMerge(HEAP *prQueue){
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "P9-heap.h"
+
+int main(){
+	HEAP *prQueue1, *prQueue2;
+	char exit, bufferCleaner;
+	CUST *cust;
+	bool result;
+	int numCusts = 0;
 	
+	prQueue1 = heapCreate(maxQueue, compareCust);
+	prQueue2 = heapCreate(maxQueue, compareCust);
+
+	printf("\n ------ Queue 1 ------");
+	printf("\n Type q to exit\n");
+	printf(" Type y to continue adding\n\n");
+
+	do{
+		cust = getCust();
+		cust->serial = cust->priority * 1000 + (1000 - numCusts);
+		result = heapInsert(prQueue1, cust);
+
+		printf("Keep adding?\n");
+		scanf("%c%c", &bufferCleaner, &exit);
+	} while(exit != 'q');
+
+	printf("\n ------ Queue 2 ------");
+	printf("\n Type q to exit\n");
+	printf(" Type y to continue adding\n\n");
+
+	do{
+		cust = getCust();
+		cust->serial = cust->priority * 1000 + (1000 - numCusts);
+		result = heapInsert(prQueue2, cust);
+
+		printf("Keep adding?\n");
+		scanf("%c%c", &bufferCleaner, &exit);
+	} while(exit != 'q');
+
+	return 0;
 }
